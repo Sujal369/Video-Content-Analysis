f5173f785471b8be67e85948fbf89e366adf6abb
Sujal Lothe <130586852+Sujal369@users.noreply.github.com>
2024-12-17 11:55:27 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white text-center p-10">
+      <h1 className="text-4xl font-bold">Tailwind CSS is working!</h1>
+    </div>
+  );
+}
+
+export default App;
